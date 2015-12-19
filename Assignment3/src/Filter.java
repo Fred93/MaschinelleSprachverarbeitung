3f8bfee0a7e05b6b9687c941218b8b2d971dc763
oleksost <oleks.ostapenko@t-online.de>
2015-12-19 16:32:20 +0100

    Erster Versuch mit Weka

---
+++ Assignment3/src/Filter.java
@@ -0,0 +1,15 @@
+import weka.core.*;
+ import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+import weka.core.converters.ConverterUtils.DataSource;
+
+
+public class Filter {
+	
+
+	
+}
