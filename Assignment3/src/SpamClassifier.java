b028a99419cac19b34c096fca406279901fa2751
oleksost <oleks.ostapenko@t-online.de>
2015-12-22 18:34:58 +0100

    Email classification based on subject

---
+++ Assignment3/src/SpamClassifier.java
@@ -0,0 +1,319 @@
+
+ 
+import java.io.*;
+import weka.core.*;
+import weka.core.converters.TextDirectoryLoader;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.classifiers.Classifier;
+import weka.filters.*;
+import weka.core.*;
+import weka.core.converters.*;
+import weka.classifiers.trees.*;
+import weka.filters.Filter;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SpamClassifier {
+	static FastVector records;
+	static Instances trainingSet;
+	static Attribute emailMessage;
+	
+	//create Arff file from path
+	public Instances loadTextstoArff(String path) throws IOException{
+	TextDirectoryLoader loader = new TextDirectoryLoader();
+    loader.setDirectory(new File(path));
+    Instances dataRaw = loader.getDataSet();
+	return dataRaw;
+	
+	}
+	public void writeArf(Instances arff, String directory) throws IOException{
+	  BufferedWriter writer = new BufferedWriter(new FileWriter(directory));
+	    writer.write(arff.toString());
+	    writer.flush();
+	    writer.close();
+	
+	
+	}
+	
+	 private void readTrainingDataset(String dataset) throws IOException {
+
+         ArrayList<String> fileNames = new ArrayList<String>();
+         ArrayList<String> dataFiles = this.listFiles(dataset, fileNames);
+         String data = "";
+         String emailSubject="";
+         String content ="";
+         for (int looper = 0; looper < dataFiles.size(); looper++) {
+                 data = this.readFileAsString(dataFiles.get(looper));
+        	     //System.out.println(data);
+                 emailSubject=extractEmailSubject(data);
+                 content=extractEmailContent(data);
+                 
+                 Instance rec = new Instance(2);
+                 
+                 if (dataFiles.get(looper).contains("ham")) {
+                         rec.setValue((Attribute) records.elementAt(0), "ham");
+                 } else{
+                         rec.setValue((Attribute) records.elementAt(0), "spam");
+                 }
+                rec.setValue((Attribute) records.elementAt(1), emailSubject);
+                //rec.setValue((Attribute) records.elementAt(2), content);
+                 trainingSet.add(rec);
+         }
+ }
+	 
+	 private String extractEmailSubject(String file){
+		 String subject ="";
+		 String regEx = Pattern.quote("Subject:") + "(.*?)(Date:|MiME-Version:|Sender:|MIME-Version:|X-Info:|Message-Id:|Content-Type:)";
+		 Pattern pattern = Pattern.compile(regEx, Pattern.DOTALL);
+		 Matcher matcher = pattern.matcher(file);
+		 if (matcher.find())
+		 {
+		     System.out.println("Subject : "+matcher.group(1));
+		    subject=matcher.group(1);
+		 }
+		 return subject;
+	 }
+	 
+	 private String extractEmailContent(String filee){
+		 String content ="";
+		 String file = filee.toLowerCase();
+		 //nicht alle haben content-type
+		 String contentTyperegEs = Pattern.quote("content-type:") + "(.*?)\n";
+		 Pattern pattern = Pattern.compile(contentTyperegEs);
+		 Matcher matcherCT = pattern.matcher(file);
+		 if (matcherCT.find())
+		 {
+			 System.out.println(matcherCT.group(1));
+			 if(matcherCT.group(1).contains("text/plain")|| matcherCT.group(1).contains("multipart/mixed")){
+				 //content alles nach der ersten leeren Zeile
+				 String contentRegEx ="(?m)^\\s*$(.*?)\\z";
+				 Pattern patternContent = Pattern.compile(contentRegEx, Pattern.DOTALL);
+				 Matcher matcherContent = patternContent.matcher(file);
+				 if (matcherContent.find())
+				 {
+				    //System.out.println(matcherContent.group(1));
+					//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
+			      content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n", "");
+				 }
+				 
+			 } else if(matcherCT.group(1).contains("text/html"))
+			 {
+				 System.out.println("BAM");
+				 //suche html-tag teil, isz nicht immer <html>
+				 String contentRegEx ="(?iu)<[a-z]*>(.*?)\\z";
+				 Pattern patternContent = Pattern.compile(contentRegEx, Pattern.DOTALL);
+				 Matcher matcherContent = patternContent.matcher(file);
+				 if (matcherContent.find())
+				 {
+				//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
+				 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n", "");
+				 System.out.println(content);
+				 }
+				
+				 
+			 }
+		   
+		 } else
+			 //wenn kein content-type
+			 if (file.contains("<html>"))
+		 {
+				 String contentRegEx ="(?iu)<[a-z]*>(.*?)\\z";
+				 Pattern patternContent = Pattern.compile(contentRegEx, Pattern.DOTALL);
+				 Matcher matcherContent = patternContent.matcher(file);
+				 if (matcherContent.find())
+				 {
+				//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
+				 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n", "");
+				 System.out.println(content);
+			 
+		 }}else{
+			 //content alles nach der ersten leeren Zeile
+			 String contentRegEx ="(?m)^\\s*$(.*?)\\z";
+			 Pattern patternContent = Pattern.compile(contentRegEx, Pattern.DOTALL);
+			 Matcher matcherContent = patternContent.matcher(file);
+			 if (matcherContent.find())
+			 {
+			    //System.out.println(matcherContent.group(1));
+				//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
+		      content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n", "");
+			 }
+		 }
+		 return content;
+	 }
+	 
+	 
+	 private String readFileAsString(String filePath) throws IOException {
+		 File file = new File(filePath);
+         byte[] buffer = new byte[(int) file.length()];
+         BufferedInputStream f = null;
+         try {
+                 f = new BufferedInputStream(new FileInputStream(filePath));
+                 f.read(buffer);
+         } finally {
+                 if (f != null)
+                         try {
+                                 f.close();
+                         } catch (IOException ignored) {
+                         }
+         }
+         return new String(buffer);
+ }
+
+	 
+	 
+	 private ArrayList<String> listFiles(String path, ArrayList<String> fileNames) {
+         File dir = new File(path);
+         File[] files = dir.listFiles();
+
+         for (int loop = 0; loop < files.length; loop++) {
+                 if (files[loop].isDirectory()) {
+                         listFiles(files[loop].getAbsolutePath(), fileNames);
+                 } else
+                         fileNames.add(files[loop].getAbsolutePath());
+         }
+         //Collections.shuffle(fileNames);
+         return fileNames;
+ }
+ 
+ 
+  public static void main(String[] args) throws Exception {
+ 
+	SpamClassifier classifier = new SpamClassifier();
+	//Instances arff = classifier.loadTextstoArff("mails_train");
+	//classifier.writeArf(arff, "mails_raw.arff");
+	
+	    
+	 //emailMessage = new Attribute("emailMessage", (FastVector) null);
+	 Attribute emailSubject = new Attribute("emailSubject", (FastVector) null);
+     FastVector emailClass = new FastVector(2);
+     emailClass.addElement("spam");
+     emailClass.addElement("ham");
+     Attribute eClass= new Attribute("emailClass", emailClass);
+
+     records = new FastVector(2);
+     records.addElement(eClass);
+     records.addElement(emailSubject);
+     //records.addElement(emailMessage);
+
+     trainingSet = new Instances("SpamClsfyTraining", records, 40);
+     trainingSet.setClassIndex(0);
+
+
+     classifier.readTrainingDataset("mails_test");
+     classifier.writeArf(trainingSet, "test.arff");
+     
+     
+	
+	
+	
+	
+	
+	    
+	  /*
+	   * 
+	  //reading existing the arff file
+	  BufferedReader reader = new BufferedReader(
+              new FileReader("mails.arff"));
+       Instances data = new Instances(reader);
+       reader.close();
+       // setting class attribute
+       data.setClassIndex(data.numAttributes() - 1);
+       */
+       
+       // apply the StringToWordVector
+       // (see the source code of setOptions(String[]) method of the filter
+       // if you want to know which command-line option corresponds to which
+       // bean property)
+      
+     
+       
+       StringToWordVector filter = new StringToWordVector();
+       filter.setInputFormat(trainingSet);
+       Instances dataFiltered = Filter.useFilter(trainingSet, filter);
+   	   System.out.println(dataFiltered.classAttribute());  
+       classifier.writeArf(trainingSet, "mails_filtered.arff");
+       
+   	     
+   	      
+       //System.out.println(" Filtered data: " + dataFiltered); 
+       
+       //write instance dataFiltered into a arff file  
+	    
+	   
+	    
+	    // train J48 and output model
+	    //J48 classifier = new J48();
+	    //classifier.buildClassifier(dataFiltered);
+	    //System.out.println(" Classifier model: " + classifier);
+	    
+	    //Data preprocessing should be apply here, e.g. apply AttributeSelection Filters
+	    
+	    //cross Validation
+	    
+	  /*  Random rand = new Random(1); 
+	    Instances randData = new Instances(dataFiltered);  
+	    randData.randomize(rand); // randomize data with number generator
+	    
+	    
+	    for (int n = 0; n < 5; n++) {
+	    	   Instances train = randData.trainCV(5, n);
+	    	   Instances test = randData.testCV(5, n);
+	    	 
+	    	   // further processing, classification, etc.
+	    	
+	    	 }
+	    
+	    */
+	  
+	   
+	    
+	    int seed  = 10;
+	    int folds = 5;
+
+	    // randomize data
+	    Random rand = new Random(seed);
+	    Instances randData = new Instances(dataFiltered);
+	    randData.randomize(rand);
+	    if (randData.classAttribute().isNominal())
+	      randData.stratify(folds);
+
+	   
+
+	    
+	    
+	    // perform cross-validation
+	    Evaluation eval = new Evaluation(randData);
+	    for (int n = 0; n < folds; n++) {
+	      Instances train = randData.trainCV(folds, n);
+	      Instances test = randData.testCV(folds, n);
+	      // the above code is used by the StratifiedRemoveFolds filter, the
+	      // code below by the Explorer/Experimenter:
+	      // Instances train = randData.trainCV(folds, n, rand);
+
+	      // build and evaluate classifier
+	      Classifier cModel = (Classifier)new NaiveBayes();
+	      cModel.buildClassifier(train);
+	      eval.evaluateModel(cModel, test);
+	    }
+
+	    // output evaluation
+	    System.out.println();
+	    System.out.println("=== Setup ===");
+	    //System.out.println("Classifier: " + Utils.toCommandLine(cls));
+	    System.out.println("Dataset: " + dataFiltered.relationName());
+	    System.out.println("Folds: " + folds);
+	    System.out.println("Seed: " + seed);
+	    System.out.println();
+	    System.out.println(eval.toSummaryString("=== " + folds + "-fold Cross-validation ===", false));
+	    
+	
+       
+  
+  }
+}
