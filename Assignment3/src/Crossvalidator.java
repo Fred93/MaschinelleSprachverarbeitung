95b122390f2493a02b0ab99f3f489753b30e5a64
oleksost <oleks.ostapenko@t-online.de>
2015-12-28 10:52:37 +0100

    cross validation stratification

---
+++ Assignment3/src/Crossvalidator.java
@@ -0,0 +1,149 @@
+import java.util.Random;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class Crossvalidator {
+
+	
+	
+	public static void validate(int folds, Instances dataFiltered) throws Exception{
+	  
+    /*
+      
+     // Another Cross validation
+   double precision =0;
+	  double recall = 0;
+	  double fmeasure =0;
+	  double error = 0;
+	  
+	  int size =  dataFiltered.numInstances() /10;
+	  System.out.println("Number of Instances " +size);
+	  System.out.println("Number of Attributes " +dataFiltered.numAttributes());
+	  System.out.println("Class Attribute " +dataFiltered.classAttribute());
+	  
+	  int beginn =0;
+	  int end = size-1;
+	  
+	  for ( int i=1; i<=10; i++){
+		  System.out.println("Iteration "+i);
+		  Instances training = new Instances(dataFiltered);
+		  Instances testing = new Instances(dataFiltered, beginn, (end-beginn));
+		  for (int j=0; j<(end-beginn); j++){
+			  //System.out.println("Length training: "+ training.numInstances() +" "+training.instance(beginn));
+			  training.delete(beginn);
+			 
+		  }
+		  System.out.println("Length training "+ training.numInstances());
+		  System.out.println("Length testing "+ testing.numInstances());
+		  
+		  NaiveBayes tree = new NaiveBayes();
+		  tree.buildClassifier(training);
+		  Evaluation eveluation = new Evaluation(training);
+		  eveluation.evaluateModel(tree, testing);
+		  
+		  System.out.println("P: "+eveluation.precision(0));
+		  System.out.println("R: "+ eveluation.recall(0));
+		  System.out.println("F: " + eveluation.fMeasure(0));
+		  System.out.println("E: " + eveluation.errorRate());
+		  
+		  
+	        precision += eveluation.precision(0);
+		    recall += eveluation.recall(0);
+		    fmeasure += eveluation.recall(0);
+		    error += eveluation.errorRate();
+		    
+		    beginn = end +1;
+		    end+=size;
+		    if (i==9){
+		    	end=dataFiltered.numInstances();
+		    }
+		  
+	  }
+	  System.out.println("Precision: "+precision/10.0);
+	  System.out.println("Recall: "+ recall/10.0);
+	  System.out.println("fMeasure: " + fmeasure/10.0);
+	  System.out.println("Error: " + error/10.0);
+
+	  */
+	  
+	  
+	    int seed  = 10;
+	    
+	    // randomize data
+	    Random rand = new Random(seed);
+	    Instances randData = new Instances(dataFiltered);
+	    
+	    randData.randomize(rand);     
+	    
+	    if (randData.classAttribute().isNominal()){
+	      randData.stratify(folds);
+	     
+	    }
+	    // perform cross-validation
+	    Evaluation eval = new Evaluation(randData);
+	    for (int n = 0; n < folds; n++) {
+	      Instances train = randData.trainCV(folds, n);
+	      //classifier.writeArf(train, "train"+n);
+	      Instances test = randData.testCV(folds, n);
+	      
+	      
+	      //check stratification
+	      int numspam=0;
+	      int numham=0;
+	      for (int a=0; a<test.numInstances(); a++){
+	    	  
+	      System.out.println(test.instance(a));
+	      if (test.instance(a).stringValue(0)=="spam"){
+	    	  numspam++;
+	      }else numham++;
+	      
+	      }
+	      System.out.println("Spam / Ham = "+numspam/numham);
+	      
+	      
+	      
+	      // the above code is used by the StratifiedRemoveFolds filter, the
+	      // code below by the Explorer/Experimenter:
+	      // Instances train = randData.trainCV(folds, n, rand);
+
+	      // build and evaluate classifier
+	      Classifier cModel = (Classifier)new NaiveBayes();
+	      cModel.buildClassifier(train);
+	      eval.evaluateModel(cModel, test);
+	      
+	      System.out.println("Fold Number"+n);
+	      System.out.println("P Fold:"+eval.precision(0));
+	      System.out.println("E Fod:"+eval.errorRate());
+	      System.out.println("R Fod:"+eval.recall(0));
+	      System.out.println("fMeasure Fold:"+eval.fMeasure(0));
+	      
+	      System.out.println("TRUE POSITIVES "+eval.numTruePositives(0));
+	      System.out.println("TRUE Negatives "+eval.numTrueNegatives(0));
+	      System.out.println("Correct Total"+eval.correct());
+	      
+	      System.out.println("Train length "+train.numInstances());
+	      System.out.println("Test length "+test.numInstances());
+	      System.out.println(" ");
+	      
+	    }
+
+	    // output evaluation
+	    System.out.println();
+	    System.out.println("=== Setup ===");
+	    //System.out.println("Classifier: " + Utils.toCommandLine(cls));
+	    System.out.println("Dataset: " + dataFiltered.relationName());
+	    System.out.println("Folds: " + folds);
+	    System.out.println("Seed: " + seed);
+	    System.out.println();
+	    System.out.println(eval.toSummaryString("=== " + folds + "-fold Cross-validation ===", false));
+
+}
+	
+	
+
+	
+}
--- Assignment3/src/SpamClassifier.java
+++ Assignment3/src/SpamClassifier.java
@@ -61,7 +61,7 @@ public class SpamClassifier {
                  } else  if (dataFiles.get(looper).contains("spam")){
                          rec.setValue((Attribute) records.elementAt(0), "spam");
                  }
-                 rec.setValue((Attribute) records.elementAt(1), emailSubject + content);
+                 rec.setValue((Attribute) records.elementAt(1), emailSubject+content);
                
                 //rec.setValue((Attribute) records.elementAt(2), content);
                  trainingSet.add(rec);
@@ -70,12 +70,13 @@ public class SpamClassifier {
 	 
 	 private String extractEmailSubject(String file){
 		 String subject ="";
-		 String regEx = Pattern.quote("Subject:") + "(.*?)(Date:|MiME-Version:|Sender:|MIME-Version:|X-Info:|Message-Id:|Content-Type:)";
+		 //ungeschickt
+		 String regEx = Pattern.quote("Subject:") + "(.*?)(Date:|MiME-Version:|X-Mailer:|X-Priority:|X-List-Unsubscribe:|\\z|Sender:|Sender:|MIME-Version:|To:|\n\n(\n)*|X-Info:|Message-Id:|Content-Type:)";
 		 Pattern pattern = Pattern.compile(regEx, Pattern.DOTALL);
 		 Matcher matcher = pattern.matcher(file);
 		 if (matcher.find())
 		 {
-		     System.out.println("Subject : "+matcher.group(1));
+		    //System.out.println("Subject : "+matcher.group(1));
 		    subject=matcher.group(1);
 		 }
 		 return subject;
@@ -90,7 +91,7 @@ public class SpamClassifier {
 		 Matcher matcherCT = pattern.matcher(file);
 		 if (matcherCT.find())
 		 {
-			 System.out.println(matcherCT.group(1));
+			 //System.out.println(matcherCT.group(1));
 			 if(matcherCT.group(1).contains("text/plain")|| matcherCT.group(1).contains("multipart/mixed")){
 				 //content alles nach der ersten leeren Zeile
 				 String contentRegEx ="(?m)^\\s*$(.*?)\\z";
@@ -100,7 +101,7 @@ public class SpamClassifier {
 				 {
 				    //System.out.println(matcherContent.group(1));
 					//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
-					 content=matcherContent.group(1).replaceAll("(<(.|\n)*?>)|([0-9])|[^\\w\\s]|((?m)^\\s*$)", "");
+					 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n\n(\n*)", "");
 						
 				 }
 				 
@@ -114,10 +115,10 @@ public class SpamClassifier {
 				 if (matcherContent.find())
 				 {
 				//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
-				 content=matcherContent.group(1).replaceAll("(<(.|\n)*?>)|([0-9])|[^\\w\\s]|((?m)^\\s*$)", "");
+					 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n\n(\n*)", "");
 				
 				 
-				 System.out.println("Ohne Sonderzeichen : "+content);
+				 //System.out.println("Ohne Sonderzeichen : "+content);
 				 }
 				
 				 
@@ -133,8 +134,8 @@ public class SpamClassifier {
 				 if (matcherContent.find())
 				 {
 				//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
-				 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n", "");
-				 System.out.println(content);
+				 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n\n(\n*)", "");
+				// System.out.println(content);
 			 
 		 }}else{
 			 //content alles nach der ersten leeren Zeile
@@ -145,7 +146,7 @@ public class SpamClassifier {
 			 {
 			    //System.out.println(matcherContent.group(1));
 				//Lösche alle html tags, linebreaks,leere Zeilen, zahlen
-		      content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n", "");
+				 content=matcherContent.group(1).replaceAll("(<((.|\n)*?)>)|([0-9])|=|((?m)^\\s*$)|\n\n(\n*)", "");
 			 }
 		 }
 		 return content;
@@ -202,32 +203,24 @@ public class SpamClassifier {
      records = new FastVector(2);
      records.addElement(eClass);
      records.addElement(emailSubject);
-   
-    
+
      //records.addElement(emailMessage);
      trainingSet = new Instances("SpamClsfyTraining", records, 80);
+     System.out.println("TrainingSet length "+trainingSet.numInstances());
      trainingSet.setClass(eClass);
      classifier.readTrainingDataset("mails_test_subset");
      classifier.writeArf(trainingSet, "test.arff");
-     
-    
 	/*
 	BufferedReader breader = null;
 	breader = new BufferedReader(new FileReader("mails_naive.arff"));
 	trainingSet=new Instances (breader);
 	*/
-
-	    
-	  
-      
-     
-       
        StringToWordVector filter = new StringToWordVector();
        filter.setLowerCaseTokens(true);
-       //filter.setTFTransform(true);
-       //filter.setIDFTransform(true);
-       //filter.setOutputWordCounts(true);
-       //trainingSet.setClass(eClass);
+       filter.setTFTransform(true);
+       filter.setIDFTransform(true);
+       filter.setOutputWordCounts(true);
+       trainingSet.setClass(eClass);
        filter.setInputFormat(trainingSet);
        Instances dataFiltered = Filter.useFilter(trainingSet, filter);
        //write to file
@@ -241,119 +234,9 @@ public class SpamClassifier {
    	      
        //System.out.println(" Filtered data: " + dataFiltered); 
        
+       Crossvalidator.validate(10, dataFiltered);
       
 	    
 	   
-	  
-       /*
-         
-        // Another Cross validation
-      double precision =0;
-	  double recall = 0;
-	  double fmeasure =0;
-	  double error = 0;
-	  
-	  int size =  dataFiltered.numInstances() /10;
-	  System.out.println("Number of Instances " +size);
-	  System.out.println("Number of Attributes " +dataFiltered.numAttributes());
-	  System.out.println("Class Attribute " +dataFiltered.classAttribute());
-	  
-	  int beginn =0;
-	  int end = size-1;
-	  
-	  for ( int i=1; i<=10; i++){
-		  System.out.println("Iteration "+i);
-		  Instances training = new Instances(dataFiltered);
-		  Instances testing = new Instances(dataFiltered, beginn, (end-beginn));
-		  for (int j=0; j<(end-beginn); j++){
-			  //System.out.println("Length training: "+ training.numInstances() +" "+training.instance(beginn));
-			  training.delete(beginn);
-			 
-		  }
-		  System.out.println("Length training "+ training.numInstances());
-		  System.out.println("Length testing "+ testing.numInstances());
-		  
-		  NaiveBayes tree = new NaiveBayes();
-		  tree.buildClassifier(training);
-		  Evaluation eveluation = new Evaluation(training);
-		  eveluation.evaluateModel(tree, testing);
-		  
-		  System.out.println("P: "+eveluation.precision(0));
-		  System.out.println("R: "+ eveluation.recall(0));
-		  System.out.println("F: " + eveluation.fMeasure(0));
-		  System.out.println("E: " + eveluation.errorRate());
-		  
-		  
-	        precision += eveluation.precision(0);
-		    recall += eveluation.recall(0);
-		    fmeasure += eveluation.recall(0);
-		    error += eveluation.errorRate();
-		    
-		    beginn = end +1;
-		    end+=size;
-		    if (i==9){
-		    	end=dataFiltered.numInstances();
-		    }
-		  
-	  }
-	  System.out.println("Precision: "+precision/10.0);
-	  System.out.println("Recall: "+ recall/10.0);
-	  System.out.println("fMeasure: " + fmeasure/10.0);
-	  System.out.println("Error: " + error/10.0);
-
-	  */
-	  
-	  
-	    int seed  = 10;
-	    int folds = 10;
-	    // randomize data
-	    Random rand = new Random(seed);
-	    Instances randData = new Instances(dataFiltered);
-	    
-	    randData.randomize(rand);
-	    classifier.writeArf(randData, "mails_random.arff");
-	       
-	    
-	    if (randData.classAttribute().isNominal()){
-	      randData.stratify(folds);
-	     
-	    }
-	    // perform cross-validation
-	    Evaluation eval = new Evaluation(randData);
-	    for (int n = 0; n < folds; n++) {
-	      Instances train = randData.trainCV(folds, n);
-	      //classifier.writeArf(train, "train"+n);
-	      Instances test = randData.testCV(folds, n);
-	      // the above code is used by the StratifiedRemoveFolds filter, the
-	      // code below by the Explorer/Experimenter:
-	      // Instances train = randData.trainCV(folds, n, rand);
-
-	      // build and evaluate classifier
-	      Classifier cModel = (Classifier)new NaiveBayes();
-	      cModel.buildClassifier(train);
-	      eval.evaluateModel(cModel, test);
-	      
-	      System.out.println("Fold "+n);
-	      System.out.println("P:"+eval.precision(0));
-	      System.out.println("E:"+eval.errorRate());
-	      System.out.println("R:"+eval.recall(0));
-	      System.out.println("F:"+eval.fMeasure(0));
-	      
-	      System.out.println("Train length "+train.numInstances());
-	      System.out.println("Test length "+test.numInstances());
-	      System.out.println(" ");
-	      
-	    }
-
-	    // output evaluation
-	    System.out.println();
-	    System.out.println("=== Setup ===");
-	    //System.out.println("Classifier: " + Utils.toCommandLine(cls));
-	    System.out.println("Dataset: " + dataFiltered.relationName());
-	    System.out.println("Folds: " + folds);
-	    System.out.println("Seed: " + seed);
-	    System.out.println();
-	    System.out.println(eval.toSummaryString("=== " + folds + "-fold Cross-validation ===", false));
-  
   }
 }
